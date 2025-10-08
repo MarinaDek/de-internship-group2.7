3e5c4f1c87a7252e24f60ffb0d6b5e8acc0fc2d8
DanyK <daniilkalibera@gmail.com>
2025-10-08 23:41:21 +0900

    Dany task

---
+++ Dany_1.py
@@ -0,0 +1,22 @@
+# Угадайте что это за алгоритм
+
+def fib(n):
+    result = []
+    for i in range(n+1):
+        if i == 0:
+            result.append(0)
+        elif i == 1:
+            result.append(1)
+        else:
+            result.append(result[i - 1] + result[i - 2])
+    return result[-1]
+
+def main():
+    n = int(input())
+    print(fib(n))
+
+
+if __name__ == "__main__":
+    main()
+
+# Ваши ответы пишите тут ниже
